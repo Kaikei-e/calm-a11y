5b46b9a08fcd9deec910c57f36e09915968d929e
Kaikei <e_kaikei@protonmail.com>
2025-10-01 22:24:40 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('Mounted');
+	});
+</script>
+
+
+<h1>Calm A11y: Monitor Your Health, Live</h1>
+<p>This is a health monitoring tool that helps you track your health and well-being.</p>
+<p>これはヘルストラッキングツールで、あなたの健康と良い生活の実現をサポートします。</p>
+
